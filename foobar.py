2fa4bd93cc55672f47cb2439cd3c40b7f5f91287
gondov <atanas.gondov@gmail.com>
2023-08-21 18:46:59 +0300

    Update foobar.py

--- foobar.py
+++ foobar.py
@@ -1,42 +1,44 @@
-ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
-DAG_ID = "postgres_operator_dag"
+import psycopg2
 
-with DAG(
-    dag_id=DAG_ID,
-    start_date=datetime.datetime(2020, 2, 2),
-    schedule="@once",
-    catchup=False,
-) as dag:
-    create_pet_table = PostgresOperator(
-        task_id="create_pet_table",
-        sql="""
-            CREATE TABLE IF NOT EXISTS pet (
-            pet_id SERIAL PRIMARY KEY,
-            name VARCHAR NOT NULL,
-            pet_type VARCHAR NOT NULL,
-            birth_date DATE NOT NULL,
-            OWNER VARCHAR NOT NULL);
-          """,
-    )
-    populate_pet_table = PostgresOperator(
-        task_id="populate_pet_table",
-        sql="""
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Max', 'Dog', '2018-07-05', 'Jane');
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Susie', 'Cat', '2019-05-01', 'Phil');
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Lester', 'Hamster', '2020-06-23', 'Lily');
-            INSERT INTO pet (name, pet_type, birth_date, OWNER)
-            VALUES ( 'Quincy', 'Parrot', '2013-08-11', 'Anne');
-            """,
-    )
-    get_all_pets = PostgresOperator(task_id="get_all_pets", sql="SELECT * FROM pet;")
-    get_birth_date = PostgresOperator(
-        task_id="get_birth_date",
-        sql="SELECT * FROM pet WHERE birth_date BETWEEN SYMMETRIC %(begin_date)s AND %(end_date)s",
-        parameters={"begin_date": "2020-01-01", "end_date": "2020-12-31"},
-        hook_params={"options": "-c statement_timeout=3000ms"},
+import datetime
+
+from airflow.decorators import dag
+
+from airflow.operators.empty import EmptyOperator
+
+ 
+
+@dag()
+def test_db_conn():
+
+    # connect to the DB
+
+    conn = psycopg2.connect(
+
+        host="172.16.1.18",
+
+        port=5432,
+
+        database="etl",
+
+        user="postgres",
+
+        password="postgres"
+
     )
 
-    create_pet_table >> populate_pet_table >> get_all_pets >> get_birth_date
+    cur = conn.cursor()
+
+ 
+
+    # fetch data
+
+    fetch_all = "SELECT * FROM omop54.care_site"
+
+    results = cur.execute(fetch_all)
+
+    print(results)
+
+ 
+
+test_db_conn()
