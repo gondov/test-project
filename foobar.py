a1cceb1f3ced75c140bb9ce4bd683de5a7bfa533
gondov <atanas.gondov@gmail.com>
2023-08-22 16:41:05 +0300

    Update foobar.py

--- foobar.py
+++ foobar.py
@@ -6,7 +6,7 @@ from airflow.operators.empty import EmptyOperator
 
 @dag(
         schedule = '0 1 * * *',
-        start_date = pendulum.datetime(2021,1,1, tz="UTC"),
+        
         catchup=False,
         tags=["Example"],
 )
