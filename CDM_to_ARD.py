886bcb7becca06cb50c0d92f485e549c7439704e
gondov <atanas.gondov@gmail.com>
2023-08-25 15:44:55 +0300

    Update CDM_to_ARD.py

--- CDM_to_ARD.py
+++ CDM_to_ARD.py
@@ -12,7 +12,7 @@ default_args = {
 }
 
 dag = DAG(
-    'dbt_dag',
+    'CDM_to_ARD',
     default_args=default_args,
     schedule_interval=None,  # Set your desired schedule interval
     catchup=False,  # Set to True if you want to backfill
